6f4a674e1a041be6f8c8d1c492f7d1ca99745878
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2021-11-10 17:25:45 +0100

    Added ISCED statics

--- src/models/publishing/isced.ts
+++ src/models/publishing/isced.ts
@@ -1,23 +1,28 @@
 import { model, Schema } from 'mongoose'
 import { iscedInterface, iscedModel } from './interfaces'
 
-const iscedSchema = new Schema<iscedInterface>(
-  {
-    level: {
-      type: Number,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
+const iscedSchema = new Schema<iscedInterface>({
+  level: {
+    type: Number,
+    required: true,
   },
-  {
-    _id: false,
-  }
-)
+  description: {
+    type: String,
+    required: true,
+  },
+})
 
 iscedSchema.index({ level: 1 })
+
+iscedSchema.statics.getISCED = async function (level: number) {
+  const isced = await this.findOne({ level })
+  return isced
+}
+
+iscedSchema.statics.getISCEDS = async function () {
+  const isceds = await this.find()
+  return isceds
+}
 
 const Isced = model<iscedInterface, iscedModel>('Isced', iscedSchema, 'isceds')
 
